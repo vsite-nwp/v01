707b31a7b67595a7ef92dc1e8e713e60e8663e63
Nenad Caklovic <nenad.caklovic@avl.com>
2015-10-09 21:19:31 +0200

    Added project files.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+	}
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+
+int RegisterMyClass(HINSTANCE hInstance, char* className)
+{
+	WNDCLASS wc;
+	ZeroMemory(&wc, sizeof wc);
+
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+
+	return RegisterClass(&wc);
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
+{
+	char clsName[] = "NWPClass";
+
+	if(!RegisterMyClass(hInstance, clsName))
+		return 0;
+
+	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL); 
+
+	MSG msg;
+	while(GetMessage(&msg, NULL, 0, 0))
+		DispatchMessage(&msg);
+
+	return msg.wParam;
+}
+
