3d4f17f4e0440e8e8dcc4d94718d0ae3ad9df370
kaCulek <karlo2316@gmail.com>
2015-10-12 20:52:29 +0200

    Added message boxes...

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ void OnCreate(HWND hw) {
 
 void OnCommand(HWND hw, int id) {
 	// TODO: show message box with text depending on which button was pressed
+	switch (id) {
+	case id_button1:
+		MessageBox(hw, "one", "NWP", MB_OK);
+		break;
+	case id_button2:
+		MessageBox(hw, "two", "NWP", MB_OK);
+		break;
+	}
 }
 
 void OnDestroy() {
