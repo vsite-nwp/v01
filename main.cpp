605f60d087910705933e53503e798eb89dc0bc82
gajotres100 <pongo666>
2015-10-12 20:50:12 +0200

    Dodan OnCreate i OnComand

--- main.cpp
+++ main.cpp
@@ -3,11 +3,24 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("BUTTON", "ONE", WS_CHILD | WS_VISIBLE,
+		100, 100, 100, 30,
+		hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("BUTTON", "TWO", WS_CHILD | WS_VISIBLE,
+		100, 200, 100, 30,
+		hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed alkjhfkjshfkj
+	switch (id)
+	{
+		case id_button1:
+			MessageBox(hw, "one", "Clicked", MB_OK);
+			break;
+		case id_button2:
+			MessageBox(hw, "two", "Clicked", MB_OK);
+			break;
+	}
 }
 
 void OnDestroy() {
