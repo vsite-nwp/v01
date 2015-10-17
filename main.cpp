dd4d976c1b261f472f0ab3e5e1a7ca9cfc19e5bd
Bojan Pađen <page0itv@gmail.com>
2015-10-17 21:41:45 +0200

    + CreateSolidBrush fun za promjenu bg bojeu cyan

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = (HBRUSH) CreateSolidBrush(RGB(0, 255, 255)); // TODO: replace with cyan background
 
 	return RegisterClass(&wc);
 }
