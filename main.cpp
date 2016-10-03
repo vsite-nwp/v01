408dbd1751477a90cecd5e970aa66a16236fd7e2
Renato <Renato.Katalenic@outlook.com>
2016-10-04 00:04:00 +0200

    Added two buttons and messagebox.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE, 100, 150, 100, 50, hw, (HMENU)id_button1, NULL, NULL);
+	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE, 100, 210, 100, 50, hw, (HMENU)id_button2, NULL, NULL);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	if (id == id_button1)
+		MessageBox(hw, "one", "button", MB_ICONWARNING | MB_DEFBUTTON1);
+	else if (id == id_button2)
+		MessageBox(hw, "two", "button", MB_ICONWARNING | MB_DEFBUTTON1);
 }
 
 void OnDestroy() {
@@ -43,7 +47,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = (HBRUSH) CreateSolidBrush(RGB(0, 255, 255)); // TODO: replace with cyan background
 
 	return RegisterClass(&wc);
 }
