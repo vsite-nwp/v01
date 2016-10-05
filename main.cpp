40670c94217e293199794463358165d1c6abaef8
Korisnik <Korisnik@HOBBES>
2016-10-05 12:24:24 +0200

    Having some fun with buttons....

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
 	// TODO: create two child windows of type button
-	CreateWindow("BUTTON", "first", WS_CHILD | WS_VISIBLE, 100, 100, 100, 50, hw, (HMENU) id_button1, 0, 0);
-	CreateWindow("BUTTON", "second", WS_CHILD | WS_VISIBLE, 100, 200, 100, 50, hw, (HMENU) id_button2, 0, 0);
+	CreateWindow("BUTTON", "first", WS_CHILD | WS_VISIBLE, 200, 100, 100, 50, hw, (HMENU) id_button1, 0, 0);
+	CreateWindow("BUTTON", "second", WS_CHILD | WS_VISIBLE, 200, 250, 100, 50, hw, (HMENU) id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
 	// TODO: show message box with text depending on which button was pressed
 	if (id == id_button1)
-	MessageBox(NULL, "first", NULL, MB_OK);
+		MessageBox(NULL, "prvi! prvi! prvi!", "First button says::", MB_OK);
 	if (id == id_button2)
-	MessageBox(NULL, "second", NULL, MB_OK);
+		MessageBox(NULL, "drugi :(", "Second button says::", MB_OK);
 
 }
 
@@ -63,7 +63,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 		return 0;
 
 	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, 500, 450,
 		NULL, NULL, hInstance, NULL); 
 
 	MSG msg;
