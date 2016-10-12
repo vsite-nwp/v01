28545fa9c16386975bb4ae3ee6175d8b45aa4b5e
KapustaB <kapusta.borna.s@gmail.com>
2016-10-12 19:47:39 +0200

    drugi komit

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void OnCreate(HWND hw) {
 void OnCommand(HWND hw, int id) {
 	if (id == id_button1)
 		MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
-	else
+	else if (id == id_button2)
 		MessageBox(hw, "two", "NWP", MB_OK | MB_ICONWARNING);
 }
 
