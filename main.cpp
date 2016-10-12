3ca0e5b1efaed5952a7bed63a73ae4b7c26501e7
KapustaB <kapusta.borna.s@gmail.com>
2016-10-12 19:42:25 +0200

    prvi komit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("BUTTON", "one", WS_CHILDWINDOW | WS_VISIBLE, 80, 50, 60, 25, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("BUTTON", "two", WS_CHILDWINDOW | WS_VISIBLE, 80, 80, 60, 25, hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	if (id == id_button1)
+		MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
+	else
+		MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
 }
 
 void OnDestroy() {
@@ -18,15 +22,15 @@ LRESULT CALLBACK WndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-		case WM_CREATE:
-			OnCreate(hw);
-			return 0;
-		case WM_COMMAND:
-			OnCommand(hw, LOWORD(wp));
-			return 0;
-		case WM_DESTROY:
-			OnDestroy();
-			return 0;
+	case WM_CREATE:
+		OnCreate(hw);
+		return 0;
+	case WM_COMMAND:
+		OnCommand(hw, LOWORD(wp));
+		return 0;
+	case WM_DESTROY:
+		OnDestroy();
+		return 0;
 	}
 	return DefWindowProc(hw, msg, wp, lp);
 }
@@ -43,7 +47,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 255, 255));
 
 	return RegisterClass(&wc);
 }
@@ -53,15 +57,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	char clsName[] = "NWPClass";
 
-	if(!RegisterMyClass(hInstance, clsName))
+	if (!RegisterMyClass(hInstance, clsName))
 		return 0;
 
-	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+	HWND hwnd = CreateWindow(clsName, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL); 
+		NULL, NULL, hInstance, NULL);
 
 	MSG msg;
-	while(GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, NULL, 0, 0))
 		DispatchMessage(&msg);
 
 	return msg.wParam;
