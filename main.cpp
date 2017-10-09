1af96339f947a4ca96f4232f499793b394930516
Matija Horvat <horvat2112@gmail.com>
2017-10-09 20:24:26 +0200

    sve gotovo

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+
+	CreateWindow("BUTTON", "Child 1", WS_VISIBLE|WS_CHILD, 100, 100, 100, 30, hw, (HMENU)id_button1, 0, 0);
+
+	CreateWindow("BUTTON", "Child 2", WS_VISIBLE|WS_CHILD, 100, 200, 100, 30, hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+
+	switch (id) {
+	case id_button1:
+		MessageBox(hw, "Button 1", "NWP", MB_OK);
+		return;
+	case id_button2:
+		MessageBox(hw, "Button 2", "NWP", MB_OK);
+		return;
+	}
+
 }
 
 void OnDestroy() {
@@ -43,7 +55,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0,255,255));
 
 	return RegisterClass(&wc);
 }
