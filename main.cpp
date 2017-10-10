deaa4c7f9bc3e9f2c0f28922cbdc955a71c3ba1d
Marin Pepeljnjak <marin.pepeljnjak@vsite.hr>
2017-10-10 21:15:25 +0200

    novi rijeseni

--- main.cpp
+++ main.cpp
@@ -15,10 +15,10 @@ void OnCommand(HWND hw, int id) {
 	switch (id)
 	{
 	case id_button1:
-		MessageBox(hw, "one", "NWP", MB_OK);
+		MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
 		break;
 	case id_button2:
-		MessageBox(hw, "two", "NWP",MB_OK);
+		MessageBox(hw, "two", "NWP",MB_OK | MB_ICONWARNING);
 		break;
 	}
 }
