c200e1dfaabb3dd5cf52a0fb31e4fb04f39886af
Ana-Marija Kostanjčar <anamarija.kostanjcar@gmail.com>
2017-10-10 19:16:42 +0200

    1vj

--- main.cpp
+++ main.cpp
@@ -8,8 +8,13 @@ void OnCreate(HWND hw) {
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	if (id == id_button1)
+		MessageBox(hw, "one", "NWP", MB_OK|MB_ICONWARNING);
+	if (id == id_button2)
+		MessageBox(hw, "two", "NWP", MB_OK | MB_ICONWARNING);
+
 }
+
 
 void OnDestroy() {
 	PostQuitMessage(0);
