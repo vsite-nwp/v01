9a4168d96f76b55d0e21f4e02169526f318dff49
Mario Habijanec <43680701+mhabijanec@users.noreply.github.com>
2018-10-03 02:21:49 +0200

    removed TODO & button HINSTANCE

--- main.cpp
+++ main.cpp
@@ -3,17 +3,17 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	//create two child windows of type button
 	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE,
 		10, 10, 60, 35,
-		hw, (HMENU)id_button1, (HINSTANCE)GetWindowLong(hw, GWL_HINSTANCE), NULL);
+		hw, (HMENU)id_button1, NULL, NULL);
 	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE,
 		10, 50, 60, 35,
-		hw, (HMENU)id_button2, (HINSTANCE)GetWindowLong(hw, GWL_HINSTANCE), NULL);
+		hw, (HMENU)id_button2, NULL, NULL);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	//show unique message box for buttons
 	if (id == 1) MessageBox(hw, "one", "NWP", MB_ICONWARNING | MB_OK);
 	else if (id == 2) MessageBox(hw, "two", "NWP", MB_ICONWARNING | MB_OK);
 }
@@ -51,7 +51,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = CreateSolidBrush(RGB(0,255,255)); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0,255,255)); //window background colour set to cyan
 
 	return RegisterClass(&wc);
 }
