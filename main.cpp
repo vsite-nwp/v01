ab9a85d5ea44e423aac90858adcb37b45e0bcd13
Marko <maperica@vsite.hr>
2019-10-14 21:28:11 +0200

    Stavljen MessageBox

--- main.cpp
+++ main.cpp
@@ -5,14 +5,19 @@ enum { id_button1 = 1, id_button2 };
 void OnCreate(HWND hw) {
 	// TODO: create two child windows of type button
 
-	HWND button1 = CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE, 100, 100, 80, 24, hw, 0, (HINSTANCE)id_button1, 0);
+	HWND button1 = CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE, 100, 100, 80, 24, hw, (HMENU)id_button1, 0, 0);
 
-	HWND button2 = CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE, 100, 150, 80, 24, hw, 0, (HINSTANCE)id_button2, 0);
+	HWND button2 = CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE, 100, 150, 80, 24, hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
 	// TODO: show message box with text depending on which button was pressed
-
+	switch (id) {
+		case id_button1:
+			MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
+		case id_button2:
+			MessageBox(hw, "two", "NWP", MB_OK | MB_ICONWARNING);
+	}
 	
 }
 
