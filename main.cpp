1545684e64600a28403aec849d8d89ffde8ad07c
JosipVuckovic <josip.vuckovic@live.com>
2019-10-14 21:30:03 +0200

    Solution to practice 1

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("button", "one", WS_CHILD | WS_VISIBLE, 50, 50, 80, 24, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("button", "two", WS_CHILD | WS_VISIBLE, 50, 100, 80, 24, hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	switch (id)
+	{
+	case id_button1:
+		MessageBox(hw, "clicked on one", "one", MB_ICONEXCLAMATION);
+		break;
+	case id_button2:
+		MessageBox(hw, "clicked on two", "two", MB_ICONEXCLAMATION);
+		break;
+
+	default:
+		break;
+	}
 }
 
 void OnDestroy() {
@@ -18,15 +30,17 @@ LRESULT CALLBACK WndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-		case WM_CREATE:
-			OnCreate(hw);
-			return 0;
-		case WM_COMMAND:
-			OnCommand(hw, LOWORD(wp));
-			return 0;
-		case WM_DESTROY:
-			OnDestroy();
-			return 0;
+	case WM_CREATE:
+		OnCreate(hw);
+		return 0;
+	case WM_COMMAND:
+		OnCommand(hw, LOWORD(wp));
+		return 0;
+	case WM_DESTROY:
+		OnDestroy();
+		return 0;
+
+
 	}
 	return DefWindowProc(hw, msg, wp, lp);
 }
@@ -43,7 +57,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush((COLORREF)RGB(0, 255, 255));
 
 	return RegisterClass(&wc);
 }
@@ -53,15 +67,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	char clsName[] = "NWPClass";
 
-	if(!RegisterMyClass(hInstance, clsName))
+	if (!RegisterMyClass(hInstance, clsName))
 		return 0;
 
-	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+	HWND hwnd = CreateWindow(clsName, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL); 
+		NULL, NULL, hInstance, NULL);
 
 	MSG msg;
-	while(GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, NULL, 0, 0))
 		DispatchMessage(&msg);
 
 	return msg.wParam;
