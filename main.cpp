bfd90af6f60930cde20b0de21a45d34ca8159ed0
ivek81cro <ivek81cro@gmail.com>
2019-10-15 22:58:09 +0200

    Removed TO DO comments Removed extra HBRUSH cast

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) 
-{
-	// TODO: create two child windows of type button
+{	
 	CreateWindow("BUTTON", "Button 1", WS_CHILD | WS_VISIBLE, 150, 100, 100, 30, hw, (HMENU)id_button1, 0, 0);
 	CreateWindow("BUTTON", "Button 2", WS_CHILD | WS_VISIBLE, 150, 150, 100, 30, hw, (HMENU)id_button2, 0, 0);
 }
 
 void OnCommand(HWND hw, int id) 
 {
-	// TODO: show message box with text depending on which button was pressed
 	if (id == id_button1)
 		MessageBox(NULL, "First button", "First button message", MB_OK | MB_ICONEXCLAMATION);
 	if (id == id_button2)
@@ -52,7 +50,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) CreateSolidBrush(RGB(0,255,255)); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0,255,255));
 
 	return RegisterClass(&wc);
 }
