7301d32747a7bfe163f56072e45196524938670f
Senad Mandžić <semandzi@vsite.hr>
2019-10-16 19:07:30 +0200

    maknuo todo komentare, maknuo hbrush cast i sredio gumbe

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
-	CreateWindow("BUTTON", "Button1", WS_CHILD | WS_VISIBLE, 150, 150, 100, 50, hw, (HMENU)id_button1, 0, 0);
-	CreateWindow("BUTTON", "Button2", WS_CHILD | WS_VISIBLE, 150, 220, 100, 50, hw, (HMENU)id_button2, 0, 0);
+	
+	CreateWindow("BUTTON", "1", WS_CHILD | WS_VISIBLE, 150, 150, 100, 50, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("BUTTON", "2", WS_CHILD | WS_VISIBLE, 150, 220, 100, 50, hw, (HMENU)id_button2, 0, 0);
 }
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
 
-	if (id = id_button1)
-		MessageBox(NULL, "Window message", "message", MB_OK | MB_ICONEXCLAMATION);
+
+	switch (id) {
+	case id_button1:
+		MessageBox(NULL, "1", "NWP", MB_OK | MB_ICONEXCLAMATION);
+		break;
+	case id_button2:
+		MessageBox(NULL, "2", "NWP", MB_OK | MB_ICONEXCLAMATION);
+	}
 }
-
 void OnDestroy() {
 	PostQuitMessage(0);
 }
@@ -47,7 +51,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) CreateSolidBrush(RGB(0,255,255)); // TODO: replace with cyan background
+	wc.hbrBackground =  CreateSolidBrush(RGB(0,255,255));
 
 	return RegisterClass(&wc);
 }
