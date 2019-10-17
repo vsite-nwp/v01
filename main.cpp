9b8056f67bdff6fb35fd451892cdc0b5df2a6f25
Sara Cuzele <saracuzele@gmail.com>
2019-10-17 19:30:49 +0200

    Changed background color, added buttons and textboxes

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE,
+		100, 100, 100, 30,
+		hw, (HMENU)id_button1, NULL, NULL);
+
+	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE,
+		100, 200, 100, 30,
+		hw, (HMENU)id_button2, NULL, NULL);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	switch (id)
+	{
+	case id_button1: MessageBox(hw,
+		"Neki tekst!!",
+		"ONE",
+		MB_ICONEXCLAMATION | MB_OK);
+		break;
+	case id_button2: MessageBox(hw,
+		"Neki tekst!!",
+		"TWO",
+		MB_ICONEXCLAMATION | MB_OK);
+		break;
+	}
 }
 
 void OnDestroy() {
@@ -43,7 +61,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 255, 255));
 
 	return RegisterClass(&wc);
 }
@@ -52,7 +70,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	char clsName[] = "NWPClass";
-
+	
 	if(!RegisterMyClass(hInstance, clsName))
 		return 0;
 
