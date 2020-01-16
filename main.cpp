9bde7c3354d174c18f43fddc65f23925de121aa5
Ivan Mihin <ivmihin@vsite.hr>
2020-01-16 15:38:17 +0100

    Vježba01

--- main.cpp
+++ main.cpp
@@ -2,16 +2,22 @@
 
 enum { id_button1 = 1, id_button2 };
 
-void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
-	HWND hwnd1 = CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE,
-		50, 50, 100, 50, hw, NULL, NULL, NULL);
-	HWND hwnd2 = CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE,
-		50, 150, 100, 50, hw, NULL, NULL, NULL);
+void OnCreate(HWND hw) 
+{
+	CreateWindow("BUTTON", "One", WS_CHILD | WS_VISIBLE, 50, 50, 100, 50, hw, (HMENU)id_button1, NULL, NULL);
+	CreateWindow("BUTTON", "Two", WS_CHILD | WS_VISIBLE, 50, 150, 100, 50, hw, (HMENU)id_button2, NULL, NULL);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	switch (id)
+	{
+	case id_button1:
+		MessageBox(hw, "Clicked on button one", "One", MB_ICONEXCLAMATION | MB_OK);
+		break;
+	case id_button2:
+		MessageBox(hw, "Clicked on button two", "Two", MB_ICONEXCLAMATION | MB_OK);
+		break;
+	}
 }
 
 void OnDestroy() {
@@ -22,51 +28,47 @@ LRESULT CALLBACK WndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-		case WM_CREATE:
-			OnCreate(hw);
-			return 0;
-		case WM_COMMAND:
-			OnCommand(hw, LOWORD(wp));
-			return 0;
-		case WM_DESTROY:
-			OnDestroy();
-			return 0;
+	case WM_CREATE:
+		OnCreate(hw);
+		return 0;
+
+	case WM_COMMAND:
+		OnCommand(hw, LOWORD(wp));
+		return 0;
+
+	case WM_DESTROY:
+		OnDestroy();
+		return 0;
 	}
 	return DefWindowProc(hw, msg, wp, lp);
 }
-
 
 int RegisterMyClass(HINSTANCE hInstance, char* className)
 {
 	WNDCLASS wc;
 	ZeroMemory(&wc, sizeof wc);
-
 	wc.lpfnWndProc = WndProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = className;
-
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) CreateSolidBrush(RGB(0, 255, 255)); // TODO: replace with cyan background
-
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 255, 255));
 	return RegisterClass(&wc);
 }
-
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	char clsName[] = "NWPClass";
-
-	if(!RegisterMyClass(hInstance, clsName))
+	if (!RegisterMyClass(hInstance, clsName))
 		return 0;
-	
-	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+
+	HWND hwnd = CreateWindow(clsName, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL); 
+		NULL, NULL, hInstance, NULL);
 
 	MSG msg;
-	while(GetMessage(&msg, NULL, 0, 0))
-		DispatchMessage(&msg);
+	while (GetMessage(&msg, NULL, 0, 0))
 
+		DispatchMessage(&msg);
 	return msg.wParam;
 }
