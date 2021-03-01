34ffe57d2490799038ffe24df7a65dcffb89be92
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-03-01 22:40:58 +0100

    Resolve vjezba1

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 enum { id_button1 = 1, id_button2 };
 
 void OnCreate(HWND hw) {
-	// TODO: create two child windows of type button
+	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE,
+		325, 100, 150, 100, hw, (HMENU)id_button1, NULL, NULL);
+
+	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE,
+		325, 250, 150, 100,
+		hw, (HMENU)id_button2, NULL, NULL);
 }
 
 void OnCommand(HWND hw, int id) {
-	// TODO: show message box with text depending on which button was pressed
+	if (id == id_button1)
+		MessageBox(hw, "Button ONE message", "Button One", MB_OKCANCEL);
+
+	if (id == id_button2)
+		MessageBox(hw, "Button TWO message", "Button Two", MB_YESNOCANCEL);
+
 }
 
 void OnDestroy() {
@@ -43,7 +53,7 @@ int RegisterMyClass(HINSTANCE hInstance, char* className)
 
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0,255,255));
 
 	return RegisterClass(&wc);
 }
@@ -57,7 +67,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 		return 0;
 
 	HWND hwnd = CreateWindow(clsName, "NWP 1",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
 		NULL, NULL, hInstance, NULL); 
 
 	MSG msg;
