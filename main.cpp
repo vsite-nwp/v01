d316f89359f8456c4a048ed4b94664bf9d232b14
tirupcic <tirupcic@vsite.hr>
2021-03-03 11:44:51 +0100

    Resolve button MB_OK

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ void OnCreate(HWND hw) {
 void OnCommand(HWND hw, int id) {
 
 	if (id == id_button1) {
-		MessageBox(hw, "one", "NWP", MB_ICONWARNING | MB_DEFBUTTON2);
+		MessageBox(hw, "one", "NWP", MB_ICONWARNING | MB_OK);
 		
 	}
 	else if (id == id_button2) {
-		MessageBox(hw, "two", "NWP", MB_ICONWARNING | MB_DEFBUTTON2);
+		MessageBox(hw, "two", "NWP", MB_ICONWARNING | MB_OK);
 	}
 }
 
