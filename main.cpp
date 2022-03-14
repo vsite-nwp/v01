68d56be8346ea01205c002b8d89fa89e8ac57bf0
LukaPetranovic <lupetran@vsite.hr>
2022-03-14 14:16:01 +0100

    MessageBoxes + CreateWindow

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 
 enum { id_button1 = 1, id_button2 };
 
-void on_create(HWND hw) 
+void on_create(HWND hw)
 {
 	// TODO: create two child windows of type button
+	::CreateWindow("BUTTON", "One", WS_CHILD | WS_VISIBLE, 20, 20, 60, 20, hw, (HMENU)id_button1, 0, 0);
+	::CreateWindow("BUTTON", "Two", WS_CHILD | WS_VISIBLE, 60, 60, 60, 20, hw, (HMENU)id_button2, 0, 0);
 }
 
 void on_command(HWND hw, int id) 
 {
 	// TODO: show message box with text depending on which button was pressed
+	int id_button1 = MessageBox(NULL, "one", "NWP", MB_OK);
+	int id_button2 = MessageBox(NULL, "two", "NWP", MB_OK);
 }
 
 void on_destroy() 
@@ -43,7 +47,8 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));  // TODO: replace with cyan background
+	//wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));  // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 255, 255));
 	return ::RegisterClass(&wc);
 }
 
