bc845a0ec9465bd9b2067ab93da8a35fe8176f5c
Miomir Lovric <miomir.oe@gmail.com>
2022-04-10 14:37:03 +0200

    v1 gotov

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@ enum { id_button1 = 1, id_button2 };
 void on_create(HWND hw) 
 {
 	// TODO: create two child windows of type button
+	CreateWindow("Button", "1", WS_CHILD | WS_VISIBLE, 100, 100, 120, 30, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("Button", "2", WS_CHILD | WS_VISIBLE, 100, 150, 120, 30, hw, (HMENU)id_button2, 0, 0);
+
 }
 
 void on_command(HWND hw, int id) 
 {
 	// TODO: show message box with text depending on which button was pressed
+	if (id == id_button1)
+		MessageBox(hw, "Button 1", "Button 1", MB_ICONWARNING);
+	else
+		MessageBox(hw, "Button 2", "Button 2", MB_ICONSTOP);
 }
 
 void on_destroy() 
@@ -43,7 +50,7 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));  // TODO: replace with cyan background
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 255, 255));  // TODO: replace with cyan background
 	return ::RegisterClass(&wc);
 }
 
