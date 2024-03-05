bffb04ad7f28672ce477e927b53a5ace42a5a243
Sonja Perković <soperkov@vsite.hr>
2024-03-05 21:17:50 +0100

    popravak navedenih plus mali dodaci za zabavu

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
 #include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-enum { id_button1 = 1, id_button2 };
+enum { id_button1 = 1, id_button2, id_button3 };
 
 void on_create(HWND hw) 
 {
 	// create two child windows of type button
-	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE, 200, 200, 200, 30, hw, (HMENU)id_button1, 0, 0);
-	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE, 200, 260, 200, 30, hw, (HMENU)id_button2, 0, 0);
+	CreateWindow("BUTTON", "one", WS_CHILD | WS_VISIBLE, 150, 200, 200, 30, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("BUTTON", "two", WS_CHILD | WS_VISIBLE, 150, 260, 200, 30, hw, (HMENU)id_button2, 0, 0);
+	CreateWindow("BUTTON", "three", WS_CHILD | WS_VISIBLE, 150, 320, 200, 30, hw, (HMENU)id_button3, 0, 0);
 }
 
 void on_command(HWND hw, int id) 
 {
-	// TODO: show message box with text depending on which button was pressed
+	// show message box with text depending on which button was pressed
 	switch (id)
 	{
-	case 1:
+	case id_button1:
 		::MessageBox(hw, "one", "NWP", MB_OK | MB_ICONEXCLAMATION);
 		return;
 
-	case 2:
+	case id_button2:
 		::MessageBox(hw, "two", "NWP", MB_OK | MB_ICONEXCLAMATION);
 		return;
+	case id_button3:
+		srand(time(0));
+		MoveWindow(hw, rand() % 1420, rand() % 480, 500, 600, 0);
 	}
 }
 
@@ -55,7 +61,7 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::CreateSolidBrush(RGB(0,255,255)));  // replace with cyan background
+	wc.hbrBackground = ::CreateSolidBrush(RGB(0,255,255));  // replace with cyan background
 	return ::RegisterClass(&wc);
 }
 
@@ -65,7 +71,7 @@ int WINAPI WinMain(HINSTANCE hi, HINSTANCE, LPSTR cmd_line, int show_flag)
 	if (!register_class(hi, class_name))
 		return 0;
 	::CreateWindow(class_name, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hi, 0);
+		CW_USEDEFAULT, CW_USEDEFAULT, 500, 600, 0, 0, hi, 0);
 	MSG msg;
 	while (::GetMessage(&msg, 0, 0, 0))
 		::DispatchMessage(&msg);
