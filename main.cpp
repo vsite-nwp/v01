83c168c94d36d620f77f455188159448306630c9
Josip Grabušić <jograbus@vsite.hr>
2024-03-05 19:49:17 +0100

    Popravljeni nedostaci: obrisano TODO, korišteno MB_OK | MB_ICONWARNING  te je zamijenjen NULL

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <windows.h>
 
-
 enum { id_button1 = 1, id_button2 };
 
-void on_create(HWND hw) 
+void on_create(HWND hw)
 {
-	// TODO: create two child windows of type button
+	//create two child windows of type button
 	CreateWindow("BUTTON", "One", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-		50, 50, 100, 30, hw, (HMENU)id_button1, NULL, NULL);
+		50, 50, 100, 30, hw, reinterpret_cast<HMENU>(id_button1), nullptr, nullptr);
 
 	CreateWindow("BUTTON", "Two", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-		50, 100, 100, 30, hw, (HMENU)id_button2, NULL, NULL);
+		50, 100, 100, 30, hw, reinterpret_cast<HMENU>(id_button2), nullptr, nullptr);
 }
 
-void on_command(HWND hw, int id) 
+void on_command(HWND hw, int id)
 {
-	// TODO: show message box with text depending on which button was pressed
+	//show message box with text depending on which button was pressed
 	LPCTSTR lpText = nullptr;
 	LPCTSTR lpCaption = nullptr;
 
@@ -35,11 +34,11 @@ void on_command(HWND hw, int id)
 
 	if (lpText && lpCaption)
 	{
-		MessageBox(hw, lpText, lpCaption, MB_ICONWARNING);
+		MessageBox(hw, lpText, lpCaption, MB_OK | MB_ICONWARNING);
 	}
 }
 
-void on_destroy() 
+void on_destroy()
 {
 	::PostQuitMessage(0);
 }
@@ -48,19 +47,18 @@ LRESULT CALLBACK window_proc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-		case WM_CREATE:
-			on_create(hw);
-			return 0;
-		case WM_COMMAND:
-			on_command(hw, LOWORD(wp));
-			return 0;
-		case WM_DESTROY:
-			on_destroy();
-			return 0;
+	case WM_CREATE:
+		on_create(hw);
+		return 0;
+	case WM_COMMAND:
+		on_command(hw, LOWORD(wp));
+		return 0;
+	case WM_DESTROY:
+		on_destroy();
+		return 0;
 	}
 	return ::DefWindowProc(hw, msg, wp, lp);
 }
-
 
 int register_class(HINSTANCE hi, const char* name)
 {
@@ -69,8 +67,8 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.lpszClassName = name;
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	HBRUSH hBrush = CreateSolidBrush(RGB(0, 255, 255));
+	wc.hCursor = ::LoadCursor(nullptr, IDC_ARROW);
+	HBRUSH hBrush = CreateSolidBrush(RGB(0, 255, 255)); //replace with cyan background
 	wc.hbrBackground = hBrush;
 	return ::RegisterClass(&wc);
 }
@@ -81,9 +79,9 @@ int WINAPI WinMain(HINSTANCE hi, HINSTANCE, LPSTR cmd_line, int show_flag)
 	if (!register_class(hi, class_name))
 		return 0;
 	::CreateWindow(class_name, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hi, 0);
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hi, nullptr);
 	MSG msg;
-	while (::GetMessage(&msg, 0, 0, 0))
+	while (::GetMessage(&msg, nullptr, 0, 0))
 		::DispatchMessage(&msg);
 	return msg.wParam;
 }
